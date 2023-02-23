dc5fcf360c00923055fc222884d7357e1d0dcb02
Danny <oluwaremidaniel36@gmail.com>
2023-02-23 11:11:35 -0800

    Fixing a bug

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,32 @@
 #include "main.h"
-void print_integer(int m);
 
 /**
- * print_number - a function that prints an integer.
- * @n: An input integer
- * Return: Nothing
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: An input integer
+ * Return: Always 0
  */
-void print_number(int n)
+void print_triangle(int size)
 {
-	if (n == 0)
-		_putchar('0');
-	else if (n < 0)
+	int i = 0, j, n = size - 1;
+
+	if (size > 0)
 	{
-		_putchar('-');
-		print_integer(n * -1);
+		for (; i < size; i++)
+		{
+			
+			for (j = 0; j < size; j++)
+			{
+				
+				if (j < n)
+					_putchar(' ');
+				
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
 	}
 	else
-		print_integer(n);
-}
-
-/**
- * print_integer - A function to priting n
- * @m: an input unsigned integer
- * Return: Nothing
- */
-void print_integer(int m)
-{
-	int i = 1000000000;
-
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
+		_putchar('\n');
 }
