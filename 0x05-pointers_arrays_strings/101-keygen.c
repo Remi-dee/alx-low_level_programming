271433f9ddee1fc7a5250008b3f4fa346ee35725
Danny <oluwaremidaniel36@gmail.com>
2023-02-27 11:37:22 -0800

    Fixing a Bug

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,15 +6,15 @@
 
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
+    char password[PASSWORD_LENGTH + 1]; 
     int i;
 
-    srand(time(NULL)); // initialize random seed with current time
+    srand(time(NULL)); 
 
     for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = '!' + rand() % ('~' - '!' + 1); // generate random printable ASCII character
+        password[i] = '!' + rand() % ('~' - '!' + 1); 
     }
-    password[PASSWORD_LENGTH] = '\0'; // add null terminator to the end of the password
+    password[PASSWORD_LENGTH] = '\0'; 
 
     printf("%s\n", password);
 
