25ce5f5995b640188f452fa4ccedc5e96b5fdc86
Danny <oluwaremidaniel36@gmail.com>
2023-03-03 15:24:33 -0800

    Fixing the bug

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
-/**
- * _strncat - a function that concatenates two strings.
- * @dest: an input string
- * @src: an input string
- * @n: an input integer
- * Return: A pointer to the resulting string
- */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int srclen = 0, i = 0;
 	char *temp = dest, *start = src;
-
+		/*Using a While loop*/
 	while (*src)
 	{
 		srclen++;
