bdb9875929f6b151fd2159ebdc64ae5b9cf25af9
Danny <oluwaremidaniel36@gmail.com>
2023-03-07 12:51:37 -0800

    fixing a bug

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,8 +8,10 @@
  * Return:  a pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
  */
+
 char *_strstr(char *haystack, char *needle)
 {
+
 	char *startn = needle, *starth = haystack;
 
 	while (*haystack)
