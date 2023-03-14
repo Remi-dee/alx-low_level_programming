b38dab2d2806d9519cbb5480a86eeac3cb0dfdc9
Danny <oluwaremidaniel36@gmail.com>
2023-03-14 05:05:34 -0700

    Fixing a bug

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * _strdup - it returns a pointer to a newly allocated
+ * _strdup - returns a pointer to a newly allocated
  *space in memory, which contains a copy of the
  *string given as a parameter.
- *@str:String to be duplicate
+ *@str:String to be copied
  *
  * Return: NULL in case of error, pointer to allocated
  * space
  *
  * FUNCTIONALITY
- * 1. First, we check if the string is NULL.  If true, we return NULL.
+ *
+ * 1. First, we check if the string is NULL.  If it is, we return NULL.
  * 2.  Next, we loop through the string to find the length of the string.
  * 3.  We allocate memory for the copy of the string.
- * 4.  We check if the memory was allocated.  If false, we return NULL.
+ * 4.  We check if the memory was allocated.  If it wasn’t, we return NULL.
  * 5.  We loop through the string again, this time copying the string into the new memory.
  * 6.  We add a null terminator to the end of the string.
- * 7.  We return the copy.
+ * 7.  We return the copy of the string.
  *
  * Time Complexity: O(n)
  */
